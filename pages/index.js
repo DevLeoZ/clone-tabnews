fc146865be80fc119ed581f8dd82aa003d8c20ed
Leonardo Alves <101026687+DevLeoZ@users.noreply.github.com>
2024-10-03 23:24:18 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Relaxa meu chapa, que a vitoria ta um aço!</h1>;
 }
 
-function Teste() {
-  console.log("teste");
-}
-
-function Teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
